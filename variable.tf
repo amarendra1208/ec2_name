b8350b749b3d627c8059564371f4d48c2d1cbbd3
amarendra1208 <108320428+amarendra1208@users.noreply.github.com>
2025-01-10 23:07:38 -0600

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,18 @@
+variable "ami" {
+   type        = string
+   description = "Ubuntu AMI ID"
+   default     = "ami-0b4624933067d393a"
+}
+
+variable "instance_type" {
+   type        = string
+   description = "Instance type"
+   default     = "t2.micro"
+}
+
+variable "name_tag" {
+   type        = string
+   description = "Name of the EC2 instance"
+   default     = "Terraform_ec2"
+#default     = "${params.EC2_NAME}"
+}
