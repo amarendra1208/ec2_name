1666fef770be967df1da2da9d2f532d83c503063
amarendra1208 <108320428+amarendra1208@users.noreply.github.com>
2025-01-10 23:11:12 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,6 +7,13 @@ terraform {
   }
 }
 
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
+}
+
+
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
