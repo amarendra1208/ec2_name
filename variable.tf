0e1e050ae20bbafecf1dece5bdadb0ba99bb2827
amarendra1208 <108320428+amarendra1208@users.noreply.github.com>
2025-01-11 14:47:52 -0600

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -32,5 +32,6 @@ variable "instance_type" {
 variable "name_tag" {
    type        = string
    description = "Name of the EC2 instance"
+    default     = ""
 #default     = "${params.EC2_NAME}"
 }
