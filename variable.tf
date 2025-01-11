8b4665fad970c3a736c157f248cd60735ed0c6e3
amarendra1208 <108320428+amarendra1208@users.noreply.github.com>
2025-01-11 00:20:21 -0600

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,3 +1,16 @@
+
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  default     = ""
+}
+
 variable "ami" {
    type        = string
    description = "Ubuntu AMI ID"
